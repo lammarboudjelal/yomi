4e8e7f28af6f3efce01a5c1a67f3a02a681958a4
lammarboudjelal <linammarboudjelal@gmail.com>
2026-02-15 18:48:25 +0100

    feat: ajout de l'interface Livre

---
+++ models/Livre.ts
@@ -0,0 +1,20 @@
+export interface Livre {
+  id: number;
+  titre: string;
+  isbn: string | null;
+  resume: string | null;
+  nombre_pages: number | null;
+  edition: string | null;
+  date_publication: string | null;
+  couverture: string | null;
+  type: string;
+  etat_lecture: string;
+  note: number;
+  avis: string | null;
+  date_debut_lecture: string | null;
+  date_fin_lecture: string | null;
+  statut_possession: string;
+  prix: number | null;
+  auteurs: string[];
+  genres: string[];
+}
