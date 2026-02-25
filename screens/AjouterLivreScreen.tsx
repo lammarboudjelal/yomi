c2e6eeac103a764aa0e2dbcc95bf45ac0a93290f
lammarboudjelal <linammarboudjelal@gmail.com>
2026-02-25 15:39:54 +0100

    feat: ajout du bouton de saisie manuelle

--- screens/AjouterLivreScreen.tsx
+++ screens/AjouterLivreScreen.tsx
@@ -1,5 +1,30 @@
-import { View, Text } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+function BoutonSaisieManuelle() {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: "white",
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        elevation: 6,
+        borderRadius: 5,
+        padding: 15,
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 15,
+      }}
+    >
+      <Entypo name="keyboard" size={24} color="black" />
+
+      <Text style={{ fontSize: 16, fontWeight: 600 }}>Saisir manuellement</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function AjouterLivreScreen() {
   const insets = useSafeAreaInsets();
@@ -14,6 +39,8 @@ export default function AjouterLivreScreen() {
       }}
     >
       <Text style={{ fontSize: 25, fontWeight: "bold" }}>Ajouter un livre</Text>
+
+      <BoutonSaisieManuelle />
     </View>
   );
 }
